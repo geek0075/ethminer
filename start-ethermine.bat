5090c6f668463987c5a8be67be18d40f6516d4e2
Kay Taiwo <kay_taylor@outlook.com>
2017-08-11 16:18:33 -0400

    first commit

---
+++ start-ethermine.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+ethminer.exe --farm-recheck 200 -G -S us1.ethermine.org:4444 -FS eu1.ethermine.org:4444 -O 0x8c5887c6908acc8a55ab9fb4e2e05dac5bd557c6.rig1
